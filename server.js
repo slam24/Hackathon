09cef67b0067e3ae3dbb0885f5ee85eae3c4763a
Luis Alfredo Martínez Sánchez <luismartinez242011@gmail.com>
2018-10-12 21:22:51 -0600

    feat: Add Payload URL

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ var port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.post('/listeningwebhook', function (req, res) {
+  res.send(JSON.stringify(req))
+})
+
 app.listen(port, () => console.log('Example app listening on port 3000!'))
