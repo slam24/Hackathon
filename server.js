d8a9a4ad5237a19fe3b3a037cde80460873010f1
Luis Alfredo Martínez Sánchez <luismartinez242011@gmail.com>
2018-10-12 21:44:01 -0600

    refactor: Payload URL

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/listeningwebhook', function (req, res) {
 	console.log(req.body)
+	res.send('ok');
 })
 
 app.listen(port, () => console.log('Example app listening on port 3000!'))
