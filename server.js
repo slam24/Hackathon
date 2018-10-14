45db8ee242253333e8663ea2509d76553fc9ffc1
Luis Alfredo Martínez Sánchez <luismartinez242011@gmail.com>
2018-10-14 16:29:41 -0600

    refactor: Custom routes

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
 const express = require('express')
-const app = express()
 const http = require('http')
+const bodyParser = require('body-parser')
+
+const router = require('./routes.js')
+
+const app = express()
 const server = http.createServer(app)
 const io = require('socket.io').listen(server)
-
-const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
-
-const port = process.env.PORT || 3000
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
-
-io.on('connection', (socket) => {
-  console.log('a user connected')
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
-  })
-})
-
-app.post('/listeningwebhook', function (req, res) {
-  console.log(req.body)
-  io.emit('listening webhook', JSON.parse(req.body.payload));
-  res.send('ok')
-})
+app.use('/', router)
 
 server.listen(port, () => console.log('Example app listening on port 3000!'))
