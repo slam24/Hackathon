0410fd5c585e45c9cce8241b41cbbd551f5ac9e0
Luis Alfredo Martínez Sánchez <luismartinez242011@gmail.com>
2018-10-18 22:06:56 -0600

    refactor: routes and outputpath client

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent }      from './home/home.component';
  
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: HomeComponent }
 ];
  
 @NgModule({
